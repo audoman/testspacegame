b87d05c3a801c2b57cf2c3537afa04ea573f9044
adam aurisch <adam.aurisch@gmail.com>
2021-09-13 15:07:27 +1000

    new color picker

--- Index.php
+++ Index.php
@@ -4,6 +4,9 @@
 
 <h1>Developer News</h1>
 
+<label for="exampleColorInput" class="form-label">Color picker</label>
+<input type="color" class="form-control form-control-color" id="exampleColorInput" value="#563d7c" title="Choose your color">
+
 <?php echo "The Best PHP Examples"; ?>
 
 </body>
