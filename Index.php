9924a5efe769b1bcaccf59d61e69a7690fc33535
adam aurisch <adam.aurisch@gmail.com>
2021-09-13 23:27:52 +1000

    h1

--- Index.php
+++ Index.php
@@ -21,7 +21,7 @@ $conn = mysqli_connect($servername, $username, $password);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+echo "<h1>Connected successfully</h1>";
 ?>
 </body>
 </html> 
